182820a3638dc0f600f59cf93da63283154732a4
Ale <alejandrabotacura@outlook.com.ar>
2023-04-29 19:30:54 -0300

    First steps on c# this is an example of how if else works

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if (daysUntilExpiration <= 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration <= 1)
+{
+    discountPercentage=20;
+    Console.WriteLine("Your subscription expires within a day!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage=10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+if (discountPercentage>0) {
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}// Por que guardo este codigo? me parece que fue una interesante forma de aplicar la solucion ponendo la menor cantidad de caracteres en memoria :D
+//why i saved this? cus i think that is a very good way to apply to the question using less memory
